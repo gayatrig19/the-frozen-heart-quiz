824c09c4e483da0a913cc9c9276d70806fe98529
gayatrig19 <>
2023-09-05 18:51:51 +0000

    Format questions and answers to display in array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,154 +1,82 @@
 // Questions are taken from: https://triviawhizz.com/trivia-questions/frozen and
 // https://www.funtrivia.com/search2_act.cfm?type=quizzes&searchstring=frozen+2&search1=Go%21
 // Questions for the quiz are stored below.
+//Define an array of quiz questions
 
-const questions = [{
-    question: "What fairy tale by Hans Christian Andersen inspired the 2013 animated film 'Frozen'?",
-    answers: [
-        { optionA: 'The Ice Queen' },
-        { optionB: 'The Cold Queen' },
-        { optionC: 'The Frozen Queen' },
-        { optionD: 'The Snow Queen' }
-    ],
-    correctAnswer: 'The Snow Queen'
-},
-{
-    question: "Which character is the secondary antagonist of the film 'Frozen'?",
-    answers: [
-        { optionA: 'Prince Of Weselton' },
-        { optionB: 'King Of Weselton' },
-        { optionC: 'Duke Of Weselton' },
-        { optionD: 'Count Of Weselton' }
-    ],
-    correctAnswer: 'Duke Of Weselton'
-},
-{
-    question: "Which song in the film ends with the line, 'The cold never bothered me anyway'?",
-    answers: [
-        { optionA: 'Frozen Heart' },
-        { optionB: 'Love Is An Open Door' },
-        { optionC: 'Let It Go' },
-        { optionD: 'For The First Time In Forever' }
-    ],
-    correctAnswer: 'Let It Go'
-},
-{
-    question: "Who voiced the role of Queen Iduna in 'Frozen 2'?",
-    answers: [
-        { optionA: 'Julia Roberts' },
-        { optionB: ' Ellen Degeneres' },
-        { optionC: ' Mindy Kaling' },
-        { optionD: 'Evan Rachel Wood' }
-    ],
-    correctAnswer: 'Evan Rachel Wood'
-},
-{
-    question: "Which character is the main antagonist of the film 'Frozen'?",
-    answers: [
-        { optionA: 'Elsa' },
-        { optionB: 'Oaken' },
-        { optionC: 'Marshmallow' },
-        { optionD: 'Hans' }
-    ],
-    correctAnswer: 'Hans'
-},
-{
-    question: "What is the name of the giant icy snowman guarding Elsa's ice palace?",
-    answers: [
-        { optionA: 'Fluffy' },
-        { optionB: 'Marshmallow' },
-        { optionC: 'Cloudy' },
-        { optionD: 'Goose' }
-    ],
-    correctAnswer: 'Marshmallow'
-},
-{
-    question: "What will thaw a frozen heart?",
-    answers: [
-        { optionA: 'A kiss from a reindeer' },
-        { optionB: 'A warm hug' },
-        { optionC: 'An act of true love' },
-        { optionD: 'A kiss from a troll' }
-    ],
-    correctAnswer: 'An act of true love'
-},
-{
-    question: "Which character in the film is known for his 'Yoo - hoo! Big summer blowout!' quote?",
-    answers: [
-        { optionA: 'Kristoff' },
-        { optionB: 'Olaf' },
-        { optionC: 'Oaken' },
-        { optionD: 'Pabbie' }
-    ],
-    correctAnswer: 'Oaken'
-},
-{
-    question: "Who among the following has voiced Elsa in the movie?",
-    answers: [
-        { optionA: 'Idina Menzel' },
-        { optionB: 'Kristen Bell' },
-        { optionC: 'Josh Gad' },
-        { optionD: 'Jonathan Groff' }
-    ],
-    correctAnswer: 'Idina Menzel'
-},
-{
-    question: "What is the name of the apparent leader of the trolls who thaws Anna's frozen head?",
-    answers: [
-        { optionA: 'Grand Pooba' },
-        { optionB: 'Grimlac' },
-        { optionC: 'Grossvater' },
-        { optionD: 'Grand Pabbie' }
-    ],
-    correctAnswer: 'Grand Pabbie'
-},
-{
-    question: " Which lullaby does Queen Iduna sing to Elsa and Anna in 'Frozen 2'?",
-    answers: [
-        { optionA: 'Frozen Heart' },
-        { optionB: 'All Is Found' },
-        { optionC: 'Show Yourself' },
-        { optionD: 'Let It Go' }
-    ],
-    correctAnswer: 'All Is Found'
-},
-{
-    question: "Which nation inspired the design of the kingdom of Arendelle?",
-    answers: [
-        { optionA: 'The Netherlands' },
-        { optionB: 'Sweden' },
-        { optionC: 'Denmark' },
-        { optionD: 'Norway' }
-    ],
-    correctAnswer: 'Norway'
-},
-{
-    question: "What is the name of the wind spirit in 'Frozen 2'?",
-    answers: [
-        { optionA: 'Gale' },
-        { optionB: 'Gus' },
-        { optionC: 'Zephyr' },
-        { optionD: 'Samantha' }
-    ],
-    correctAnswer: 'Gale'
-},
-{
-    question: "Which Disney princess makes a cameo appearance at Elsa's coronation?",
-    answers: [
-        { optionA: 'Tiana' },
-        { optionB: 'Rapunzel' },
-        { optionC: 'Ariel' },
-        { optionD: 'Belle' }
-    ],
-    correctAnswer: 'Rapunzel'
-},
-{
-    question: "Which magical river is originally believed to be a myth in 'Frozen 2'?",
-    answers: [
-        { optionA: 'Iardanus' },
-        { optionB: 'Vadgelmir' },
-        { optionC: 'Ahtohallan' },
-        { optionD: 'Styx' }
-    ],
-    correctAnswer: 'Ahtohallan'
-}];
+const quizQuestions = [
+    {
+        question: "What fairy tale by Hans Christian Andersen inspired the 2013 animated film 'Frozen'?",
+        answers: ["The Ice Queen", "The Cold Queen", "The Frozen Queen", "The Snow Queen"],
+        correctAnswer: "The Snow Queen"
+    },
+    {
+        question: "Which character is the secondary antagonist of the film 'Frozen'?",
+        answers: ["Prince Of Weselton", "King Of Weselton", "Duke Of Weselton", "Count Of Weselton"],
+        correctAnswer: "Duke Of Weselton"
+    },
+    {
+        question: "Which song in the film ends with the line, 'The cold never bothered me anyway'?",
+        answers: ["Frozen Heart", "Love Is An Open Door", "Let It Go", "For The First Time In Forever"],
+        correctAnswer: "Let It Go"
+    },
+    {
+        question: "Who voiced the role of Queen Iduna in 'Frozen 2'?",
+        answers: ["Julia Roberts", "Ellen Degeneres", "Mindy Kaling", "Evan Rachel Wood"],
+        correctAnswer: "Evan Rachel Wood"
+    },
+    {
+        question: "Which character is the main antagonist of the film 'Frozen'?",
+        answers: ["Elsa", "Oaken", "Marshmallow", "Hans"],
+        correctAnswer: "Hans"
+    },
+    {
+        question: "What is the name of the giant icy snowman guarding Elsa's ice palace?",
+        answers: ["Fluffy", "Marshmallow", "Cloudy", "Goose"],
+        correctAnswer: 'Marshmallow'
+    },
+    {
+        question: "What will thaw a frozen heart?",
+        answers: ["A kiss from a reindeer", "A warm hug", "An act of true love", "A kiss from a troll"],
+        correctAnswer: "An act of true love"
+    },
+    {
+        question: "Which character in the film is known for his 'Yoo - hoo! Big summer blowout!' quote?",
+        answers: ["Kristoff", "Olaf", "Oaken", "Pabbie"],
+        correctAnswer: "Oaken"
+    },
+    {
+        question: "Who among the following has voiced Elsa in the movie?",
+        answers: ["Idina Menzel", "Kristen Bell", "Josh Gad", "Jonathan Groff"],
+        correctAnswer: "Idina Menzel"
+    },
+    {
+        question: "What is the name of the apparent leader of the trolls who thaws Anna's frozen head?",
+        answers: ["Grand Pooba", "Grimlac", "Grossvater", "Grand Pabbie"],
+        correctAnswer: "Grand Pabbie"
+    },
+    {
+        question: " Which lullaby does Queen Iduna sing to Elsa and Anna in 'Frozen 2'?",
+        answers: ["Frozen Heart", "All Is Found", "Show Yourself", "Let It Go"],
+        correctAnswer: "All Is Found"
+    },
+    {
+        question: "Which nation inspired the design of the kingdom of Arendelle?",
+        answers: ["The Netherlands", "Sweden", "Denmark", "Norway"],
+        correctAnswer: "Norway"
+    },
+    {
+        question: "What is the name of the wind spirit in 'Frozen 2'?",
+        answers: ["Gale", "Gus", "Zephyr", "Samantha"],
+        correctAnswer: "Gale"
+    },
+    {
+        question: "Which Disney princess makes a cameo appearance at Elsa's coronation?",
+        answers: ["Tiana", "Rapunzel", "Ariel", "Belle"],
+        correctAnswer: "Rapunzel"
+    },
+    {
+        question: "Which magical river is originally believed to be a myth in 'Frozen 2'?",
+        answers: ["Iardanus", "Vadgelmir", "Ahtohallan", "Styx"],
+        correctAnswer: "Ahtohallan"
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,10 @@
-// Getting the DOM elements
-//Defining variables
-
-let startButton = document.getElementById('start-button');
-let startPage = document.getElementById('start-container');
-let userNameInput = document.getElementById('user-name');
-let message = document.getElementById('message');
-let quizArea = document.getElementById('quiz-container');
+// Global variables to track the quiz state
 
 
-//Validating and Getting Username
-/**
- * Function will check for the user has enter a username before starting the quiz and 
- * the name is valid with more than 3 characters with no blank spaces. 
- * The username will be used to display final score on quiz completion.
- */
 
-let user = userNameInput;
-let validMessageEmpty = "Please enter a valid name without spaces";
-let validCharacterMessage = "Username should contain more than 3 characters";
 
-//Username Validation
-function validateUserName() {
 
-    // Code for username validation credit: https://www.youtube.com/watch?v=1iw5sdQAxAY and
-    // https://stackoverflow.com/questions/9628879/javascript-regex-username-validation
 
-    var messageLetters = /^[A-Za-z0-9]+$/;
-    // Checks for empty spaces
-    if (!user.value.match(messageLetters)) {
-        message.innerHTML = validMessageEmpty;
-        message.style.color = "red";
-        message.style.weight = "bold";
-        //checks if username entered is more than 3 characters, if true validates and starts quiz 
-    } else if (user.value.length <= Number(3)) {
-        message.innerHTML = validCharacterMessage;
-        message.style.color = "red";
-        message.style.weight = "bold";
-    } else if (user.value.length >= Number(3)) {
-        startQuiz();
-    }
-}
 
-//Event Listener for starting quiz
-startButton.onclick = () => {
-    validateUserName();
-};
 
-function startQuiz() {
-    startPage.classList.add('inactive');
-    quizArea.classList.remove('inactive');
-}
+
